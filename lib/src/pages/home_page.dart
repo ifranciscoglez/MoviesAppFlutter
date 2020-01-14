09d694a6438d665a4889aad16897dd27b5a66f4f
ifranciscoglez <fjglezn@outlook.com>
2020-01-14 12:17:53 -0600

    Se modico widget swiper

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:app_movies/src/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -28,21 +27,8 @@ class HomePage extends StatelessWidget {
   }
   
   Widget _swiperCards(){
-    return Container(
-      padding: EdgeInsets.only(top: 10.0),
-      width: double.infinity,
-      height: 300.0,
-      child: Swiper(
-        layout: SwiperLayout.STACK,
-        itemWidth: 200.0,
-        itemBuilder: (BuildContext context,int index){
-          return new Image.network("http://via.placeholder.com/350x150",
-          fit: BoxFit.fill,);
-        },
-        itemCount: 3,
-        // pagination: new SwiperPagination(),
-        // control: new SwiperControl(),
-      ),
+    return CardSwiper(
+      movies: [1, 2, 3, 4, 5],
     );
   }
 }
